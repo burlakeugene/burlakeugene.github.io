e3eb2050186c75b3f9aa24f3d3592f4c44d7f980
Eugene <eugeneburlak@yandex.ru>
2019-03-07 15:33:32 +0300

    upd

---
+++ src/components/Face/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Blob from '../Blob';
+export default class Face extends Component {
+	render() {
+		let image = require('./styles/images/me.png');
+		return (
+			<div>
+				<Blob styles={{
+					width: '200px',
+					height: '200px',
+					backgroundColor: '#000',
+					backgroundImage: 'url('+ image +')'
+				}}>
+				</Blob>
+			</div>
+		)
+	}
+}
--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import Socials from '../../components/Socials';
+import Face from '../../components/Face';
 const index = () => {
 	return (
 		<div>
 			<Socials />
+			<Face />
 		</div>
 	)
 }
