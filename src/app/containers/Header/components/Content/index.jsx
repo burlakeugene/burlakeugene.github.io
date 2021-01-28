8a65b0c6eea078c6d830745ed36e8924b69162f2
Eugene Burlak <burlakeugene@gmail.com>
2021-01-28 13:37:32 +0300

    contact links

--- src/app/containers/Header/components/Content/index.jsx
+++ src/app/containers/Header/components/Content/index.jsx
@@ -5,302 +5,47 @@ import './styles/styles.scss';
 export default class Content extends Component {
   constructor(props) {
     super(props);
-  }
-  mapInit() {
-    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
-      { map } = this.refs;
-    if (map) {
-      var mapData = {};
-      mapData.x = 44.60569;
-      mapData.y = 33.522039;
-      mapData.title = 'Title';
-      mapData.content = 'Content';
-      mapData.pinUrl = require('./images/pin.svg');
-      mapData.pinWidth = 10;
-      mapData.pinHeight = 10;
-      mapData.zoom = 10;
-      var infowindow = new google.maps.InfoWindow();
-      var myMap = new google.maps.Map(map, {
-        center: { lat: mapData.x - 0.035, lng: mapData.y },
-        zoom: mapData.zoom || 8,
-        mapTypeControl: false,
-        zoomControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        fullscreenControl: false,
-        styles: [
+    this.state = {
+      structure: {
+        title: 'Get in touch',
+        items: [
           {
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#212121'
-              }
-            ]
+            label: 'Email',
+            text: 'burlakeugene@gmail.com',
+            href: 'mailto:burlakeugene@gmail.com',
           },
           {
-            elementType: 'labels.icon',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
+            label: 'Telegram',
+            text: '@burlakeugene',
+            href: 'https://t.me/burlakeugene',
           },
-          {
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575'
-              }
-            ]
-          },
-          {
-            elementType: 'labels.text.stroke',
-            stylers: [
-              {
-                color: '#212121'
-              }
-            ]
-          },
-          {
-            featureType: 'administrative',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#757575'
-              }
-            ]
-          },
-          {
-            featureType: 'administrative.country',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#9e9e9e'
-              }
-            ]
-          },
-          {
-            featureType: 'administrative.land_parcel',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
-          },
-          {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#bdbdbd'
-              }
-            ]
-          },
-          {
-            featureType: 'administrative.neighborhood',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
-          },
-          {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575'
-              }
-            ]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#181818'
-              }
-            ]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#616161'
-              }
-            ]
-          },
-          {
-            featureType: 'poi.park',
-            elementType: 'labels.text.stroke',
-            stylers: [
-              {
-                color: '#1b1b1b'
-              }
-            ]
-          },
-          {
-            featureType: 'road',
-            elementType: 'geometry.fill',
-            stylers: [
-              {
-                color: '#2c2c2c'
-              }
-            ]
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
-          },
-          {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#8a8a8a'
-              }
-            ]
-          },
-          {
-            featureType: 'road.arterial',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#373737'
-              }
-            ]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#3c3c3c'
-              }
-            ]
-          },
-          {
-            featureType: 'road.highway.controlled_access',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#4e4e4e'
-              }
-            ]
-          },
-          {
-            featureType: 'road.local',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#616161'
-              }
-            ]
-          },
-          {
-            featureType: 'transit',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#757575'
-              }
-            ]
-          },
-          {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [
-              {
-                color: '#000000'
-              }
-            ]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text',
-            stylers: [
-              {
-                visibility: 'off'
-              }
-            ]
-          },
-          {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [
-              {
-                color: '#3d3d3d'
-              }
-            ]
-          }
-        ]
-      });
-      var marker = new google.maps.Marker({
-        position: { lat: mapData.x, lng: mapData.y },
-        map: myMap,
-        icon: {
-          url: mapData.pinUrl,
-          scaledSize: new google.maps.Size(60, 60),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(30, 30)
-        }
-      });
-      //   google.maps.event.addListener(marker, 'click', function() {
-      //     infowindow.setContent("<p>Some HTML content</p>");
-      //     infowindow.open(myMap,marker);
-      //  });
-    }
-  }
-  componentDidMount() {
-    if (window.google) {
-      this.mapInit();
-    } else {
-      setTimeout(() => {
-        this.componentDidMount();
-      }, 0);
-    }
+        ],
+      },
+    };
+    this.structure = {};
   }
   render() {
+    let { structure } = this.state;
     return (
       <div className="app-header-content">
         <div className="app-header-content-scroller">
-          <div className="app-header-content-left">
-            <div className="app-header-content-left-inner">
-              <div className="contacts">
-                <div className="contacts-item contacts-item__place">
-                  Sevastopol
+          <div className="contacts">
+            <div className="contacts-title">{structure.title}</div>
+            {structure.items.map((item) => {
+              return (
+                <div className="contacts-item">
+                  <div className="contacts-item-label">{item.label}</div>
+                  <div className="contacts-item-link">
+                    <Link
+                      to={item.href}
+                      text={item.text}
+                      external
+                      target="_blank"
+                    />
+                  </div>
                 </div>
-                <div className="contacts-item contacts-item__mail">
-                  Get in touch:{' '}
-                  <Link
-                    text="burlakeugene@gmail.com"
-                    to="mailto:burlakeugene@gmail.com"
-                    target="_blank"
-                    external={true}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="app-header-content-right">
-            <Blob>
-              <div className="map" ref="map"></div>
-            </Blob>
+              );
+            })}
           </div>
         </div>
       </div>
